9b9f45f7ec7a83247a21caab0cc5cc0bb592ab8b
Zeddzuruzuranda <raphomelvo@gmail.com>
2023-03-26 23:14:38 +0300

    register

---
+++ src/components/register.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+function Register({ goToLogin }) {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  }
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <h1>Registration Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <button type="submit">Register</button>
+      </form>
+      <button onClick={goToLogin}>Go to Login</button>
+    </div>
+  );
+}
+
+export default Register;
